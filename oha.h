b84b1751aaaba68803338ebda2ef124471e9614a
Andy Guenther <andy.guenther89@gmail.com>
2021-03-04 21:45:54 +0100

    add experimental c++ static inline build

--- oha.h
+++ oha.h
@@ -3,6 +3,7 @@
 
 #ifdef __cplusplus
 extern "C" {
+#define restrict
 #endif
 
 #include <stdbool.h>
@@ -10,9 +11,9 @@ extern "C" {
 #include <stdint.h>
 
 #ifdef OHA_INLINE_ALL
-#   define OHA_PUBLIC_API static inline __attribute__((unused))
+#define OHA_PUBLIC_API static inline __attribute__((unused))
 #else
-#   define OHA_PUBLIC_API
+#define OHA_PUBLIC_API
 #endif
 
 struct oha_key_value_pair {
@@ -56,11 +57,9 @@ struct oha_lpht_status {
 
 OHA_PUBLIC_API struct oha_lpht * oha_lpht_create(const struct oha_lpht_config * config);
 OHA_PUBLIC_API void oha_lpht_destroy(struct oha_lpht * table);
-__attribute__((pure))
-OHA_PUBLIC_API void * oha_lpht_look_up(const struct oha_lpht * table, const void * key);
+__attribute__((pure)) OHA_PUBLIC_API void * oha_lpht_look_up(const struct oha_lpht * table, const void * key);
 OHA_PUBLIC_API void * oha_lpht_insert(struct oha_lpht * table, const void * key);
-__attribute__((pure))
-OHA_PUBLIC_API void * oha_lpht_get_key_from_value(const void * value);
+__attribute__((pure)) OHA_PUBLIC_API void * oha_lpht_get_key_from_value(const void * value);
 OHA_PUBLIC_API void * oha_lpht_remove(struct oha_lpht * table, const void * key);
 OHA_PUBLIC_API bool oha_lpht_get_status(const struct oha_lpht * table, struct oha_lpht_status * status);
 OHA_PUBLIC_API void oha_lpht_clear(struct oha_lpht * table);
@@ -84,8 +83,7 @@ struct oha_bh;
 
 OHA_PUBLIC_API struct oha_bh * oha_bh_create(const struct oha_bh_config * config);
 OHA_PUBLIC_API void oha_bh_destroy(struct oha_bh * heap);
-__attribute__((pure))
-OHA_PUBLIC_API int64_t oha_bh_find_min(const struct oha_bh * heap);
+__attribute__((pure)) OHA_PUBLIC_API int64_t oha_bh_find_min(const struct oha_bh * heap);
 OHA_PUBLIC_API void * oha_bh_delete_min(struct oha_bh * heap);
 OHA_PUBLIC_API void * oha_bh_insert(struct oha_bh * heap, int64_t key);
 OHA_PUBLIC_API int64_t oha_bh_change_key(struct oha_bh * heap, void * value, int64_t new_val);
@@ -106,12 +104,16 @@ OHA_PUBLIC_API struct oha_tpht * oha_tpht_create(const struct oha_tpht_config * 
 OHA_PUBLIC_API void oha_tpht_destroy(struct oha_tpht * tpht);
 OHA_PUBLIC_API bool oha_tpht_increase_global_time(struct oha_tpht * tpht, int64_t timestamp);
 OHA_PUBLIC_API void * oha_tpht_insert(struct oha_tpht * tpht, const void * key, uint8_t timeout_slot_id);
-__attribute__((pure))
-OHA_PUBLIC_API void * oha_tpht_look_up(const struct oha_tpht * tpht, const void * key);
+__attribute__((pure)) OHA_PUBLIC_API void * oha_tpht_look_up(const struct oha_tpht * tpht, const void * key);
 OHA_PUBLIC_API void * oha_tpht_remove(struct oha_tpht * tpht, const void * key);
-OHA_PUBLIC_API int8_t oha_tpht_add_timeout_slot(struct oha_tpht * tpht, int64_t timeout, uint32_t num_elements, bool resizable);
+OHA_PUBLIC_API int8_t oha_tpht_add_timeout_slot(struct oha_tpht * tpht,
+                                                int64_t timeout,
+                                                uint32_t num_elements,
+                                                bool resizable);
 OHA_PUBLIC_API void * oha_tpht_set_timeout_slot(struct oha_tpht * tpht, const void * key, uint8_t timeout_slot_id);
-OHA_PUBLIC_API size_t oha_tpht_next_timeout_entries(struct oha_tpht * tpht, struct oha_key_value_pair * next_pair, size_t num_pairs);
+OHA_PUBLIC_API size_t oha_tpht_next_timeout_entries(struct oha_tpht * tpht,
+                                                    struct oha_key_value_pair * next_pair,
+                                                    size_t num_pairs);
 OHA_PUBLIC_API void * oha_tpht_update_time_for_entry(struct oha_tpht * tpht, const void * key, int64_t new_timestamp);
 
 // include all code as static inline functions
@@ -122,6 +124,7 @@ OHA_PUBLIC_API void * oha_tpht_update_time_for_entry(struct oha_tpht * tpht, con
 #endif
 
 #ifdef __cplusplus
+#undef restrict
 }
 #endif
 
